6449285a9307d02a5c27c296a87ea55db55cd3c8
David Sankel <dsankel@bloomberg.net>
2019-10-10 08:15:26 -0400

    [PatternMatching] Add missing forward declaration.
    Lack of this forward declaration caused a compilation error.

--- clang/include/clang/Serialization/ASTWriter.h
+++ clang/include/clang/Serialization/ASTWriter.h
@@ -66,6 +66,7 @@ class ModuleFileExtension;
 class ModuleFileExtensionWriter;
 class NamedDecl;
 class ObjCInterfaceDecl;
+class PatternStmt;
 class PreprocessingRecord;
 class Preprocessor;
 class RecordDecl;
