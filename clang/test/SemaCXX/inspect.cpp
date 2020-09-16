9d8d842cd84e341012ffa43cb4bb8eaea22bdcff
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2020-09-15 21:52:51 -0700

    [PatternMatching] Add more tests for inspect as expression-only
    - Mismatching expression type versus trailing type.
    - Bad deduction (patterns yielding different expression types).
    - Compound statements in patterns and interaction with other types

--- clang/test/SemaCXX/inspect.cpp
+++ clang/test/SemaCXX/inspect.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -fsyntax-only -verify -fpattern-matching %s
+// RUN: %clang_cc1 -fsyntax-only -verify -fpattern-matching -Wno-unused-value %s
 
 void a() {
   inspect(42) {
@@ -6,5 +6,45 @@ void a() {
   };
   inspect(b) { // expected-error {{use of undeclared identifier 'b'}}
     __ if (true) => {};
+  };
+}
+
+void b(int x) {
+  inspect(x) -> void {
+    __ => 3; // expected-error {{cannot initialize statement expression result of type 'void' with an rvalue of type 'int'}}
+  };
+}
+
+int c(int x) {
+  int w = inspect(x) -> int {
+    __ => 2.3; // expected-warning {{implicit conversion from 'double' to 'int' changes value from 2.3 to 2}}
+  };
+  return w;
+}
+
+int bar() { return 4; }
+
+void d(int x) {
+  inspect(x) -> void {
+    __ => (void)bar();
+  };
+}
+
+void e(int x) {
+  inspect(x) -> void {
+    __ => { bar(); };
+  };
+}
+
+void f(int x) {
+  inspect(x) -> int {
+    __ => { bar(); }; // expected-error {{resulting expression type 'void' must match trailing result type 'int'}}
+  };
+}
+
+void g(int x) {
+  inspect(x) -> void {
+    __ => (void)3;
+    __ => { bar(); };
   };
 }
